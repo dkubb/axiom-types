594b3cd323687a9e0290b64df2e023a328d50669
Dan Kubb <dan.kubb@gmail.com>
2013-02-23 12:09:22 -0800

    Rename constant to be more clear

--- lib/axiom/types/date_time.rb
+++ lib/axiom/types/date_time.rb
@@ -7,13 +7,13 @@ module Axiom
     class DateTime < Object
       extend ValueComparable
 
-      NSEC_IN_SECONDS = 1 - Rational(1, 10**9)
+      MAXIMUM_NSEC_IN_SECONDS = 1 - Rational(1, 10**9)
 
       primitive       ::DateTime
       coercion_method :to_datetime
 
       minimum primitive.new(1, 1, 1)
-      maximum primitive.new(9999, 12, 31, 23, 59, 59 + NSEC_IN_SECONDS)
+      maximum primitive.new(9999, 12, 31, 23, 59, 59 + MAXIMUM_NSEC_IN_SECONDS)
 
     end # class DateTime
   end # module Types
