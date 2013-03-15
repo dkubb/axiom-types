4fc4d5d0549cc7461729800d5a9917f9916dc8d7
Dan Kubb <dan.kubb@gmail.com>
2013-03-14 22:55:03 -0700

    Add YARD docs for Axiom::Types::Encodable

--- lib/axiom/types/encodable.rb
+++ lib/axiom/types/encodable.rb
@@ -5,29 +5,51 @@ module Axiom
 
     # Add encoding constraints to a type
     module Encodable
-
       if RUBY_VERSION >= '1.9'
+
+        # Hook called when module is extended
+        #
+        # Add #encoding DSL method to descendant and set the default to UTF-8.
+        #
+        # @param [Class] descendant
+        #
+        # @return [undefined]
+        #
+        # @api private
         def self.extended(descendant)
+          super
           descendant.accept_options :encoding
           descendant.encoding Encoding::UTF_8
         end
 
+        private_class_method :extended
+
+        # Finalize by setting up a primitive constraint
+        #
+        # @return [Axiom::Types::Encodable]
+        #
+        # @api private
         def finalize
           return self if frozen?
-          has_encoding(encoding)
+          has_encoding
           super
         end
 
       private
 
-        def has_encoding(encoding)
+        # Add constraint for the encoding
+        #
+        # @return [undefined]
+        #
+        # @api private
+        def has_encoding
           constraint do |object|
             object.encoding.equal?(encoding) ||
             (encoding.ascii_compatible? && object.to_s.ascii_only?)
           end
         end
-      end
 
+      end
     end # module Encodable
   end # module Types
 end # module Axiom
