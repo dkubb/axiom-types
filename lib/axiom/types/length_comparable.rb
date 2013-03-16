05861039e93074072b54c95fd4918b17d9e046dc
Dan Kubb <dan.kubb@gmail.com>
2013-03-15 23:45:08 -0700

    Add specs for Axiom::Types::LengthComparable

--- lib/axiom/types/length_comparable.rb
+++ lib/axiom/types/length_comparable.rb
@@ -6,20 +6,42 @@ module Axiom
     # Add a minimum and maximum length constraint to a type
     module LengthComparable
 
+      # Hook called when module is extended
+      #
+      # Add #minimum_length and #maximum_length DSL methods to descendant.
+      #
+      # @param [Class<Axiom::Types::Type>] descendant
+      #
+      # @return [undefined]
+      #
+      # @api private
       def self.extended(descendant)
+        super
         descendant.accept_options :minimum_length, :maximum_length
       end
 
+      # Finalize by setting up a length range constraint
+      #
+      # @return [Axiom::Types::LengthComparable]
+      #
+      # @api private
       def finalize
         return self if frozen?
-        length_between(minimum_length, maximum_length)
+        has_length_within_range
         super
       end
 
     private
 
-      def length_between(minium, maximum)
-        constraint { |object| object.length.between?(minium, maximum) }
+      # Add a constraint for a length within a range
+      #
+      # @return [undefined]
+      #
+      # @api private
+      def has_length_within_range
+        constraint do |object|
+          object.length.between?(minimum_length, maximum_length)
+        end
       end
 
     end # module LengthComparable
