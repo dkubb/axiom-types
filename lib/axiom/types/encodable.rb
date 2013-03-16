296d0ec1bec544bd1ad3a08ded4e554236fd386b
Dan Kubb <dan.kubb@gmail.com>
2013-03-16 11:17:05 -0700

    Update Axiom::Types::Encodable to make it easier for YARD to parse

--- lib/axiom/types/encodable.rb
+++ lib/axiom/types/encodable.rb
@@ -5,68 +5,71 @@ module Axiom
 
     # Add encoding constraints to a type
     module Encodable
-      if RUBY_VERSION >= '1.9'
+      # stub module for ruby 1.8
+    end
 
-        # Hook called when module is extended
-        #
-        # Add #encoding DSL method to descendant and set the default to UTF-8.
-        #
-        # @param [Class<Axiom::Types::Type>] descendant
-        #
-        # @return [undefined]
-        #
-        # @api private
-        def self.extended(descendant)
-          super
-          descendant.accept_options :encoding
-          descendant.encoding Encoding::UTF_8
-        end
+    module Encodable
 
-        private_class_method :extended
-
-        # Finalize by setting up a primitive constraint
-        #
-        # @return [Axiom::Types::Encodable]
-        #
-        # @api private
-        def finalize
-          return self if frozen?
-          ascii_compatible? ? has_ascii_compatible_encoding : has_encoding
-          super
-        end
-
-      private
-
-        # Test if the encoding is ascii compatible
-        #
-        # @return [Boolean]
-        #
-        # @api private
-        def ascii_compatible?
-          encoding.ascii_compatible?
-        end
-
-        # Add constraint for the ascii compatible encoding
-        #
-        # @return [undefined]
-        #
-        # @api private
-        def has_ascii_compatible_encoding
-          constraint do |object|
-            object.encoding.equal?(encoding) || object.to_s.ascii_only?
-          end
-        end
-
-        # Add constraint for the encoding
-        #
-        # @return [undefined]
-        #
-        # @api private
-        def has_encoding
-          constraint { |object| object.encoding.equal?(encoding) }
-        end
-
+      # Hook called when module is extended
+      #
+      # Add #encoding DSL method to descendant and set the default to UTF-8.
+      #
+      # @param [Class<Axiom::Types::Type>] descendant
+      #
+      # @return [undefined]
+      #
+      # @api private
+      def self.extended(descendant)
+        super
+        descendant.accept_options :encoding
+        descendant.encoding Encoding::UTF_8
       end
-    end # module Encodable
+
+      private_class_method :extended
+
+      # Finalize by setting up a primitive constraint
+      #
+      # @return [Axiom::Types::Encodable]
+      #
+      # @api private
+      def finalize
+        return self if frozen?
+        ascii_compatible? ? has_ascii_compatible_encoding : has_encoding
+        super
+      end
+
+    private
+
+      # Test if the encoding is ascii compatible
+      #
+      # @return [Boolean]
+      #
+      # @api private
+      def ascii_compatible?
+        encoding.ascii_compatible?
+      end
+
+      # Add constraint for the ascii compatible encoding
+      #
+      # @return [undefined]
+      #
+      # @api private
+      def has_ascii_compatible_encoding
+        constraint do |object|
+          object.encoding.equal?(encoding) || object.to_s.ascii_only?
+        end
+      end
+
+      # Add constraint for the encoding
+      #
+      # @return [undefined]
+      #
+      # @api private
+      def has_encoding
+        constraint { |object| object.encoding.equal?(encoding) }
+      end
+
+    end if RUBY_VERSION >= '1.9'
+
   end # module Types
 end # module Axiom
