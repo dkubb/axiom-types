8568c408cd2ae86739c735663ac4b95ced501fca
Dan Kubb <dan.kubb@gmail.com>
2013-03-16 23:29:31 -0700

    Add magic encoding comment to ruby source files

--- spec/support/encoding.rb
+++ spec/support/encoding.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Encoding
   UTF_8    = new
   UTF_16BE = new
--- spec/unit/axiom/types/object/class_methods/coercion_method_spec.rb
+++ spec/unit/axiom/types/object/class_methods/coercion_method_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Axiom::Types::Object, '.coercion_method' do
