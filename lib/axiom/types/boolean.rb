26b5691e27f43257176d0d59afd8524da0dada8d
Dan Kubb <dan.kubb@gmail.com>
2013-03-22 20:42:11 -0700

    Add Axiom::Types::Boolean.primitive_class?
    * This will provide Axiom::Types::Boolean.infer

--- lib/axiom/types/boolean.rb
+++ lib/axiom/types/boolean.rb
@@ -9,6 +9,20 @@ module Axiom
 
       includes true, false
 
+      # Test if the object is the correct primitive class
+      #
+      # @param [Object] object
+      #
+      # @return [Boolean]
+      #
+      # @api private
+      def self.primitive_class?(object)
+        TrueClass.singleton_class  === object ||
+        FalseClass.singleton_class === object
+      end
+
+      private_class_method :primitive_class?
+
     end # class Boolean
   end # module Types
 end # module Axiom
