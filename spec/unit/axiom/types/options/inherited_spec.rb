9e36448b77f4d3b2b14bafab0ebedad04613874c
Dan Kubb <dan.kubb@gmail.com>
2013-03-23 23:40:35 -0700

    Add spec to kill mutation in Axiom::Types::Options

--- spec/unit/axiom/types/options/inherited_spec.rb
+++ spec/unit/axiom/types/options/inherited_spec.rb
@@ -21,18 +21,44 @@ describe Axiom::Types::Options, '#inherited' do
     Class.new(object)
   end
 
-  it 'delegates to the ancestor' do
-    ancestor.should_receive(:inherited).twice
-    subject
+  context 'when the option is not set' do
+    it 'delegates to the ancestor' do
+      ancestor.should_receive(:inherited).twice
+      subject
+    end
+
+    it 'adds the accepted option to the descendant' do
+      subject
+      expect(descendant).to respond_to(:primitive, :coerce_method)
+    end
+
+    it 'sets the default value for the descendant' do
+      subject
+      expect(descendant.primitive).to be(::String)
+    end
   end
 
-  it 'adds the accepted option to the descendant' do
-    subject
-    expect(descendant).to respond_to(:primitive, :coerce_method)
-  end
+  context 'when the option is set' do
+    before do
+      def ancestor.inherited(descendant)
+        # set the option explicitly
+        descendant.instance_variable_set(:@primitive, ::Integer)
+      end
+    end
 
-  it 'sets the default value for the descendant' do
-    subject
-    expect(descendant.primitive).to be(::String)
+    it 'delegates to the ancestor' do
+      ancestor.should_receive(:inherited).twice
+      subject
+    end
+
+    it 'adds the accepted option to the descendant' do
+      subject
+      expect(descendant).to respond_to(:primitive, :coerce_method)
+    end
+
+    it 'does not set the value for the descendant when not set' do
+      subject
+      expect(descendant.primitive).to be(::Integer)
+    end
   end
 end
