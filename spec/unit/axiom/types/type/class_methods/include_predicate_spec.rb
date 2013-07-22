9a8dd5a874f7137f40f8cbe2061a78ca3626a94e
Dan Kubb <dan.kubb@gmail.com>
2013-07-22 00:50:25 -0700

    Kill mutation in Axiom::Types::Type#include?

--- spec/unit/axiom/types/type/class_methods/include_predicate_spec.rb
+++ spec/unit/axiom/types/type/class_methods/include_predicate_spec.rb
@@ -5,20 +5,20 @@ require 'spec_helper'
 describe Axiom::Types::Type, '.include?' do
   subject { object.include?(value) }
 
-  let(:value) { Object.new }
+  let(:object) do
+    Class.new(described_class) do
+      constraint(->(object) { !object.nil? })
+    end
+  end
 
   context 'when the value matches the type constraint' do
-    let(:object) { Class.new(described_class) }
+    let(:value) { Object.new }
 
     it { should be(true) }
   end
 
   context 'when the value does not match the type constraint' do
-    let(:object) do
-      Class.new(described_class) do
-        constraint(proc { false })
-      end
-    end
+    let(:value) { nil }
 
     it { should be(false) }
   end
