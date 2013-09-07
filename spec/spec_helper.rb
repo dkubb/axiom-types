9a3796668f10b25bec33bf641771b91691f1c540
Dan Kubb <dan.kubb@gmail.com>
2013-09-06 22:07:54 -0700

    Update spec helper to be similar to other gems
    * Remove unnecessary boilerplate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-require 'devtools/spec_helper'
-
 if ENV['COVERAGE'] == 'true'
   require 'simplecov'
   require 'coveralls'
@@ -12,21 +10,20 @@ if ENV['COVERAGE'] == 'true'
   ]
 
   SimpleCov.start do
-    command_name     'spec:unit'
-    add_filter       'config'
-    add_filter       'spec'
+    command_name 'spec:unit'
+
+    add_filter 'config'
+    add_filter 'spec'
+    add_filter 'vendor'
+
     minimum_coverage 100
   end
 end
 
+require 'devtools/spec_helper'
 require 'axiom-types'
 
 include Axiom::Types
-
-# Require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
-  require file.chomp('.rb')
-end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expect_with|
