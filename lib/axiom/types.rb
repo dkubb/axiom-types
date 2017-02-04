010d5f5c4fa2266ad0959c84de555658f60e4643
Tom Russell <me@tomrussell.co.uk>
2017-02-04 12:31:57 +0000

    Use Concurrent::Array, not ThreadSafe::Array
    The ThreadSafe project is unmaintained and its codebase has been absorbed into
    Concurrent ruby, an actively maintained gem. Let's use that instead.

--- lib/axiom/types.rb
+++ lib/axiom/types.rb
@@ -7,7 +7,7 @@ require 'singleton'
 
 require 'descendants_tracker'
 require 'ice_nine'
-require 'thread_safe'
+require 'concurrent'
 
 module Axiom
 
@@ -24,7 +24,7 @@ module Axiom
     Contradiction = ->(_value) { true }.freeze
 
     # Cache the type inference lookup by object
-    @inference_cache = ThreadSafe::Cache.new do |cache, object|
+    @inference_cache = Concurrent::Hash.new do |cache, object|
       type = nil
       Type.descendants.detect do |descendant|
         type = descendant.infer(object)
